dfb2ed0ab908584ef8f592ac024ddeeeee4262ed
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-01-31 18:04:54 -0700

    added 2 functions

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ var currentHour = moment().format("HH");
 function insertCurrentDay() {
     $("#moment").text(currentDay);
 };
+
+insertCurrentDay();
+
+function textNine(){
+	var dataToSaveNine = document.getElementById("inputNine").value;	
+	localStorage.setItem("data", dataToSaveNine);  
+	localStorage.getItem("data", dataToSaveNine);
+	console.log(dataToSaveNine)}
+
+function textTen(){
+	var dataToSaveTen = document.getElementById("inputTen").value;
+	localStorage.setItem("data", dataToSaveTen);
+	localStorage.getItem("data", dataToSaveTen);
+	console.log(dataToSaveTen);
+}
