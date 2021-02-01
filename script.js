0d1c7df612cb9eda737ba3ca598ad775c3fdfa2b
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-01-31 18:02:11 -0700

    added 1st function to script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ var today = moment();
 
 var currentDay = moment().format("dddd, MMMM " + "Do, YYYY " + "-- HH:mm a");
 var currentHour = moment().format("HH");
+
+function insertCurrentDay() {
+    $("#moment").text(currentDay);
